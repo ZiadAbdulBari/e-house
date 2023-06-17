66566c89f0148d969eb1d6ab35d4142f646fedf0
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2023-06-17 16:12:36 +0600

    remove: remove code

--- ecommercr-frontend/pages/index.js
+++ ecommercr-frontend/pages/index.js
@@ -2,18 +2,11 @@ import Section from "@/components/Section/Section";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import axios from "axios";
-// import Cart from "@/components/Cart/Cart";
 import MainLayout from "@/layout/MainLayout";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [products, setProducts] = useState([]);
-  // const setLocalData = ()=>{
-  //   const cart = window.localStorage.getItem('cart-product');
-  //   if(!cart){
-  //     window.localStorage.setItem('cart-product',JSON.stringify([]));
-  //   }
-  // }
   const getProduct = () => {
     const URL = "https://dummyjson.com/products";
     axios.get(URL).then((response) => {
@@ -22,14 +15,12 @@ export default function Home() {
   };
   useEffect(() => {
     getProduct();
-    // setLocalData();
   }, []);
   return (
     <MainLayout>
       <div>
         <Section sectionName="New Arrival" products={products} />
       </div>
-      {/* <Cart /> */}
     </MainLayout>
   );
 }
